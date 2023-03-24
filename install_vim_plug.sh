b51a433dff77f9b03e0976fc8c83835479814f1f
Kylian Mayanga-Fournier <mayanga.kylian@gmail.com>
2023-03-24 14:36:46 +0100

    install bash and change the execution of install_vim_plug.sh to use bash

--- install_vim_plug.sh
+++ install_vim_plug.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [[ "$OSTYPE" == "linux-gnu"* ]] || [[ "$OSTYPE" == "darwin"* ]]; then
   curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
